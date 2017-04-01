92d1a255777058c6b9a05e841f3a98c02f867340
ThanhPT <thanhptse@gmail.com>
2017-04-02 01:19:47 +0700

    insert, delete function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFire, AuthProviders, AuthMethods, FirebaseListObservable } from 'angularfire2'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  items: FirebaseListObservable<any>;
+  name: any;
+  msgVal: string = '';
+
+  constructor(public af: AngularFire) {
+    this.items = af.database.list('/messages', {
+      query: {
+        limitToLast: 50
+      }
+    });
+    this.af.auth.subscribe(auth => {
+      if (auth) {
+        this.name = auth;
+      }
+    });
+  }
+
+  login() {
+    this.af.auth.login({
+      provider: AuthProviders.Anonymous,
+      method: AuthMethods.Anonymous
+    });
+  }
+
+  Send(desc: string) {
+    this.items.push({ messages: desc });
+    this.msgVal = '';
+  }
+
+  Delete(key: any){
+    this.items.remove(key).then(_=>console.log("item deleted"));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2'
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "09aKQT0URN4VQ4hleUzaSyA4PxADTAc5JNscE09",
+  authDomain: "todoapp-2a3c7.firebaseapp.com",
+  databaseURL: "https://todoapp-2a3c7.firebaseio.com",
+  projectId: "todoapp-2a3c7",
+  storageBucket: "todoapp-2a3c7.appspot.com",
+  messagingSenderId: "882499548301"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +22,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
